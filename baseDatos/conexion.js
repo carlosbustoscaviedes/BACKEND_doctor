ae67ac2fa9f86515089244721b86104e490a0b53
carlos alberto bustos <proyectosangular09@gmail.com>
2024-04-15 08:45:38 -0500

    se edito la conexion

--- baseDatos/conexion.js
+++ baseDatos/conexion.js
@@ -7,12 +7,19 @@ const conexionDB = async() => {
 
     try{
 
-        await mongoose.connect('mongodb+srv://proyectosangular09:Ecoutores09**@angularback.d5sixhu.mongodb.net/medicos?retryWrites=true&w=majority&appName=angularBACK');
+        await mongoose.connect('mongodb+srv://proyectosangular09:Ecoutores09**@angularback.d5sixhu.mongodb.net/medicos?retryWrites=true&w=majority&appName=angularBACK', {
+            bufferCommands: false, // Deshabilita el buffering de comandos
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        
+
         console.log("se ha conectado correctamente")
 
     }catch(err){
 
-        console.log("no se pudo conectar")
+        console.log("no se pudo conectara la base de datos")
     }
 
 }
